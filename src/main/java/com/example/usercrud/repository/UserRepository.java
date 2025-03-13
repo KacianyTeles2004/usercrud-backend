3bc442bbee99aa45e3be4f6b478c2d7fa433a2a1
joconcei <joaosoaresc03@gmail.com>
2025-03-13 14:27:33 -0300

    Commit Tela Login e Cadastro

--- src/main/java/com/example/usercrud/repository/UserRepository.java
+++ src/main/java/com/example/usercrud/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.example.usercrud.repository;
 
-import com.example.usercrud.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmailAndPassword(String email, String password);
+import com.example.usercrud.model.User;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    
+    @Query("SELECT u FROM User u WHERE (u.cpf = :identificador OR u.email = :identificador) AND u.senha = :senha")
+    User findByCpfOrEmailAndSenha(@Param("identificador") String identificador, @Param("senha") String senha);
 }
