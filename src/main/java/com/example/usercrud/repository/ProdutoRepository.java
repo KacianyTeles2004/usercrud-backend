fc3a6b7ffdd9bee231ec4fb4eb6c9b46dc40c337
KacianyTeles2004 <160554104+KacianyTeles2004@users.noreply.github.com>
2025-04-11 15:07:17 -0300

    Delete src directory

--- src/main/java/com/example/usercrud/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.usercrud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.usercrud.model.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-
-}
-
